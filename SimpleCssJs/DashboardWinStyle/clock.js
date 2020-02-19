df6c10e8bc48a2a1337dd09f6f6510cc6adbc82b
borysSynowec <borys.synowec@gmail.com>
2020-02-19 02:32:20 +0100

    fixed timer

--- SimpleCssJs/DashboardWinStyle/clock.js
+++ SimpleCssJs/DashboardWinStyle/clock.js
@@ -3,15 +3,15 @@ function counting()
     var today = new Date();
 
     var hour = today.getHours();
-    if (hour<10) hour = "0" +hour();
+    if (hour<10) hour = "0" +hour;
 
     var minute = today.getMinutes();
-    if (miunte<10) minute = "0" +minute();
+    if (minute<10) minute = "0" +minute;
 
     var second = today.getSeconds();
-    if (second<10) second = "0" +second();
+    if (second<10) second = "0" +second;
 
-    document.getElementById("clock").innerHTML = hour+":"+minute+":"+second;
+    document.getElementById("timer").innerHTML = hour+":"+minute+":"+second;
 
     setTimeout("counting()",1000);
 }
